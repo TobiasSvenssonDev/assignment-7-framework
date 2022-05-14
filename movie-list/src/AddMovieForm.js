6485d22a337685d2bdcac837a0eeead8d536ebe5
TobiasSvenssonDev <tobias_svensson84@live.se>
2022-05-14 08:55:20 +0200

    Create AddMovieForm.js
    första utkast på att lägga till en film
    
    Co-Authored-By: Axel-Heinonen <81235010+Axel-Heinonen@users.noreply.github.com>

---
+++ movie-list/src/AddMovieForm.js
@@ -0,0 +1,16 @@
+export default function addMovieForm(){
+    return (
+        <fieldset>
+            <input className="form-group" placeholder="Titel här..."></input>
+            <select className="form-group">
+                <option value="" selected>Välj betyg här</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+
+        </div>
+    )
+}
